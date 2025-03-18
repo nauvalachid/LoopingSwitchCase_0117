dfe2e8b3a513a616290806bbe95fc2bc91000da1
nauvalachid <nauvalachid@gmail.com>
2025-03-18 18:59:44 +0700

    Pencacahan Naik

---
+++ file1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Pencacahan Naik
+    int i;
+    for (i = 0; i < 5; i++) {
+    cout << "Nilai i = " << i <<"Lari Keliling Lapangan" <<endl;
+    }
+    cout << "NIlai i terakhir: " << i << endl;
+}
